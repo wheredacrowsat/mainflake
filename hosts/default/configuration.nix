082bf323cb2e336a21fb841f7f5cb1f86e0c193d
wheredacrowsat <etchickadee@tutamail.com>
2025-02-16 12:32:29 -0500

    new kernel

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -19,6 +19,9 @@
  #    extraPackages = with pkgs; [ amdvlk ];
   };
 
+  # lets try this kernel
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
